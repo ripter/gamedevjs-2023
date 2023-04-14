10ac90377226f2262dfcb749a2d02c7a22af5d90
Chris <ripter001@gmail.com>
2023-04-14 03:41:29 +0000

    Title Page

--- pages/titleScreen.mjs
+++ pages/titleScreen.mjs
@@ -1,23 +1,18 @@
 import { render, html } from '../libs/uhtml/index.mjs';
-import { signal, computed, effect } from '../libs/usignal.0.9.0.js';
+// import { signal, computed, effect } from '../libs/usignal.0.9.0.js';
 
 export function pageTitleScreen(selector) {
 	const elm = document.querySelector(selector);
-	const firstName = signal('Chris');
-	const lastName = signal('Richards');
-	const fullName = computed(() => `${firstName} ${lastName}`);
+	elm.className = 'page-title-screen';
 	
-	setTimeout(() => {
-		firstName.value = 'Rose';	
-	}, 2000);
-	
-	
-	effect(() => render(elm, html`
-		<h1>Intro Page</h1>
-		<p>${`
-			Hello ${fullName.value}
-		`}</p>
-		What's your name?
-		<input type="text" onKeypress=${evt => firstName.value = `${evt.target.value}${evt.key}`} />
-	`));	
+	render(elm, html`
+		<header>
+			<h1>Life Aboard the Celestial</h1>
+			<h2>A Gamedev.JS Jam 2023</h2>
+			<h3>By <a href="https://github.com/ripter">ripter</a></h3>
+		</header>
+		<footer>
+			<button type="button">Start Game</button>	
+		</footer>
+	`);	
 }
