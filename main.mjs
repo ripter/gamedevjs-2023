eef469c649aeb3daa6a1b6bdbd25742185f95e82
Chris <ripter001@gmail.com>
2023-04-18 02:06:52 +0000

    trying page

--- main.mjs
+++ main.mjs
@@ -1,8 +1,8 @@
-import {app} from './libs/APath.js';
 // import { pageTitleScreen } from '../pages/titleScreen.mjs';
 import { pageEncounter } from '../pages/encounter.mjs';
 import { Player } from '../utils/Player.mjs';
 import { Ship } from '../utils/Ship.mjs';
+import page from '../libs/page.mjs';
 
 // Create a new Player.
 // Put it on window so everyone can use it.
@@ -17,42 +17,8 @@ window.ship = new Ship();
 // Encounter Screen
 pageEncounter('#gamearea', 'ink/encounter-astroid.json');
 
-console.log('app', app);
-app
-	.get('/test/?query=:query', function (ctx) {
-		console.log(ctx);
-		/*
-		{
-			"path": "/test/?query=value",
-			"params": {
-				"query": "value"
-			}
-		}
-		*/
-	})
-	.get('/test/:status', function (ctx) {
-		console.log(ctx);
-		/*
-		{
-			"path": "/test/OK",
-			"params": {
-				"status": "OK"
-			}
-		}
-		*/
-	});
 
-// intercept all unregistered calls
-app.get('*',
-	function (ctx, next) {
-		console.log(ctx);
-		/*
-		{
-			"path": "/whatever"
-		}
-		*/
-		next();
-	},
-	// will receive the ctx object too
-	console.error
-);
+
+page('/home', () => { 
+	console.log('home page');
+});
