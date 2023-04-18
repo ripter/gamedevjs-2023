6e9eb179ad83fddce72fd0c5aff3eda1a44690b3
Chris <ripter001@gmail.com>
2023-04-18 02:28:33 +0000

    using signal for page state

--- main.mjs
+++ main.mjs
@@ -1,8 +1,8 @@
-// import { pageTitleScreen } from '../pages/titleScreen.mjs';
+import { pageTitleScreen } from '../pages/titleScreen.mjs';
 import { pageEncounter } from '../pages/encounter.mjs';
 import { Player } from '../utils/Player.mjs';
 import { Ship } from '../utils/Ship.mjs';
-import page from '../libs/page.mjs';
+import { signal, computed, effect } from '../libs/usignal.0.9.0.js';
 
 // Create a new Player.
 // Put it on window so everyone can use it.
@@ -11,14 +11,31 @@ window.player = new Player();
 // Put it on window so everyone can use it.
 window.ship = new Ship();
 
+// keep track of which page we should show.
+const currentPage = window.currentPage = signal({
+	url: 'intro',
+	args: [],
+	// url: 'encounter',
+	// args: ['ink/encounter-astroid.json'],
+
+});
+
+// switch pages when it changes
+effect(() => {
+	const {url, args} = currentPage.value;
+	switch(url) {
+		case 'encounter': 
+			return pageEncounter('#gamearea', ...args);
+		default:
+			return pageTitleScreen('#gamearea');
+	}	
+});
+
 // Show the Title Screen.
 // pageTitleScreen('#gamearea');
 
 // Encounter Screen
-pageEncounter('#gamearea', 'ink/encounter-astroid.json');
+// 'ink/encounter-astroid.json'
 
 
 
-page('/home', () => { 
-	console.log('home page');
-});
