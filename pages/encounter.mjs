9f852df0ee974f550cdf2fa2499057e5b6f7a4c7
Chris <ripter001@gmail.com>
2023-04-19 03:00:42 +0000

    Using new Story class

--- pages/encounter.mjs
+++ pages/encounter.mjs
@@ -1,8 +1,9 @@
 import { render, html } from '../libs/uhtml/index.mjs';
 import { signal, computed, effect } from '../libs/usignal.0.9.0.js';
 
-import { loadStory } from '../utils/loadStory.mjs';
-import { parseTags } from '../utils/parseTags.mjs';
+// import { loadStory } from '../utils/loadStory.mjs';
+// import { parseTags } from '../utils/parseTags.mjs';
+import { Story } from '../utils/Story.mjs';
 import { rollDice } from '../utils/rollDice.mjs';
 import { getChoiceComponent } from '../utils/getChoiceComponent.mjs';
 
@@ -16,44 +17,67 @@ let inkStory;
  * Encounters serve as the central gameplay mechanism, providing players with an engaging challenge that tests their abilities, strategies, and luck, and ultimately determines their success or failure in the game.
 */
 export async function pageEncounter(selector, storyURL) {
-	inkStory = await loadStory(storyURL);
-	window.inkStory = inkStory; // for debugging
-	//
-	// rollDice function. Rolls dice and sets the "time" variable with the result.
-	inkStory.BindExternalFunction('rollDice', (skillName) => {
-		const skillLevel = window.player.skills[skillName];
-		const diceResults = rollDice(skillLevel);
-		inkStory.variablesState['time'] = diceResults.reduce((acc, value) => {return acc + value}, 0);
+	const story = await Story.load(storyURL, {
+		// rollDice function. Rolls dice and sets the "time" variable with the result.
+		rollDice: (skillName) => {
+			const skillLevel = window.player.skills[skillName];
+			const diceResults = rollDice(skillLevel);
+			// inkStory.variablesState['time'] = diceResults.reduce((acc, value) => {return acc + value}, 0);
+			story.setVariable('time',  diceResults.reduce((acc, value) => {return acc + value}, 0));
+		},	
+		// Updates the Ship
+		updateShip: (propName, deltaValue) => {
+			console.log('Change Ship', propName, 'by', deltaValue);
+			return "Ok damage the {freakin|strange|large} ship."	
+		},	
+		// Updates the Player
+		updatePlayer:	(propName, deltaValue) => {
+			console.log('Change Player', propName, 'by', deltaValue);
+			return "Ok damage the {amazing|cool|awesome} player."	
+		},
+		// Called when the Encounter is over.
+		onGameOver: () => {
+			console.log('onGameOver');
+		},
 	});
+	// inkStory = await loadStory(storyURL);
+	// window.inkStory = inkStory; // for debugging
+	//
+	// inkStory.BindExternalFunction('rollDice', (skillName) => {
+	// 	const skillLevel = window.player.skills[skillName];
+	// 	const diceResults = rollDice(skillLevel);
+	// 	inkStory.variablesState['time'] = diceResults.reduce((acc, value) => {return acc + value}, 0);
+	// });
 	//
 	// Updates the Ship
-	inkStory.BindExternalFunction('updateShip', (propName, deltaValue) => {
-		console.log('Change Ship', propName, 'by', deltaValue);
-		return "Ok damage the {freakin|strange|large} ship."	
-	});
+	// inkStory.BindExternalFunction('updateShip', (propName, deltaValue) => {
+	// 	console.log('Change Ship', propName, 'by', deltaValue);
+	// 	return "Ok damage the {freakin|strange|large} ship."	
+	// });
 	//
 	// Updates the Player
-	inkStory.BindExternalFunction('updatePlayer', (propName, deltaValue) => {
-		console.log('Change Player', propName, 'by', deltaValue);
-		return "Ok damage the {amazing|cool|awesome} player."	
-	});
+	// inkStory.BindExternalFunction('updatePlayer', (propName, deltaValue) => {
+	// 	console.log('Change Player', propName, 'by', deltaValue);
+	// 	return "Ok damage the {amazing|cool|awesome} player."	
+	// });
 	//
 	// Called when the Encounter is over.
-	inkStory.BindExternalFunction('onGameOver', () => {
-		console.log('onGameOver');
-	});
+	// inkStory.BindExternalFunction('onGameOver', () => {
+	// 	console.log('onGameOver');
+	// });
 	
 	const elm = document.querySelector(selector);
 	elm.className = 'page-encounter';
 
 	// run the current story
-	runFlow();
+	state.value = story.next();
+	// runFlow();
 	
 	//
 	// Render the page.
 	effect(() => {
-		const { tagState, body, choiceList, turnsLeft } = state.value;
-		const { choiceType, background, title } = tagState;
+		const { tags, body, choiceList, turnsLeft } = state.value;
+		const { choiceType, background, title } = tags;
 		const Choice = getChoiceComponent(choiceType);
 		
 		elm.style.backgroundImage = `url(./imgs/${background})`;
@@ -63,7 +87,9 @@ export async function pageEncounter(selector, storyURL) {
 				${body}	
 			</div>
 			<ul class=${`choice-list --count-${choiceList.length}`}>
-				${choiceList.map(item => Choice(item, handleChoiceClick))}
+				${choiceList.map(item => Choice(item, (choiceIdx) => {
+					state.value = story.pickChoice(choiceIdx);
+				}))}
 			</ul>
 		`);
 	});	
@@ -73,33 +99,33 @@ export async function pageEncounter(selector, storyURL) {
 /**
  * Clicking on a choice
 */
-function handleChoiceClick(choiceIdx) {
-	// Select the choice
-	inkStory.ChooseChoiceIndex(choiceIdx);
-	// Run the Story
-	runFlow();
-}
+// function handleChoiceClick(choiceIdx) {
+// 	// Select the choice
+// 	inkStory.ChooseChoiceIndex(choiceIdx);
+// 	// Run the Story
+// 	runFlow();
+// }
 
 
 /**
  * Run The InkStory
 */
-function runFlow() {
-	const text = [];
-	const tagList = [];	
-	
-	while (inkStory.canContinue) {
-		text.push(inkStory.Continue());
-		tagList.push(...inkStory.currentTags);
-	}
-	const tagState = parseTags(tagList);
-	
-	state.value = {
-		body: text.map(val => html`<p>${val}</p>`),
-		choiceList: inkStory.currentChoices,
-		tagState: {
-			...state.value.tagState,
-			...tagState,
-		}
-	};
-}
+// function runFlow() {
+// 	const text = [];
+// 	const tagList = [];	
+// 	
+// 	while (inkStory.canContinue) {
+// 		text.push(inkStory.Continue());
+// 		tagList.push(...inkStory.currentTags);
+// 	}
+// 	const tagState = parseTags(tagList);
+// 	
+// 	state.value = {
+// 		body: text.map(val => html`<p>${val}</p>`),
+// 		choiceList: inkStory.currentChoices,
+// 		tagState: {
+// 			...state.value.tagState,
+// 			...tagState,
+// 		}
+// 	};
+// }
--- utils/Story.mjs
+++ utils/Story.mjs
@@ -4,36 +4,76 @@ import { parseTags } from '../utils/parseTags.mjs';
 /**
  * Wrapper around the Ink Story
 */
-export class Story() {
-	constructor(url) {
-		this.inkStory = await loadStory(url);
+export class Story {
+	constructor() {
+		this.inkStory = null;
+		this.tagState = {};	
 	}
 	
-	handleChoiceClick(choiceIdx) {
+	/**
+	 * Loads an Ink Story by URL
+ 	*/
+	static async load(url, boundFunctions) {
+		const story = new Story();	
+		story.inkStory = await loadStory(url);
+		
+		for (const [key, value] of Object.entries(boundFunctions)) {
+			story.inkStory.BindExternalFunction(key, value);
+		}
+		
+		return story;
+	}
+	
+	/**
+	 * Returns the Story Variable
+	*/
+	getVariable(name) {
+		const { inkStory } = this;
+		return inkStory.variablesState[name];
+	}
+	/**
+	 * Sets a Story Variable
+	*/
+	setVariable(name, value) {
+		const { inkStory } = this;
+		inkStory.variablesState[name] = value;
+	}
+	
+	/**
+	 * Picks the choice in the story and returns the new state.
+	*/
+	pickChoice(choiceIdx) {
+		const { inkStory } = this;
 		// Select the choice
-		this.inkStory.ChooseChoiceIndex(choiceIdx);
+		inkStory.ChooseChoiceIndex(choiceIdx);
 		// Run the Story
 		return this.next();
 	}
 	
 	// Run the Ink Story and update the state.
 	next() {
-		const text = [];
+		const { inkStory, tagState } = this;
+		const body = [];
 		const tagList = [];	
 		
+		// Get all the body lines and tags for all of them.	
 		while (inkStory.canContinue) {
-			text.push(inkStory.Continue());
+			body.push(inkStory.Continue());
 			tagList.push(...inkStory.currentTags);
 		}
-		const tagState = parseTags(tagList);
+		// parse the raw tag text into a tags object.
+		const tags = parseTags(tagList);
+		
+		// Tags persist over the story state. 
+		this.tagState = {
+			...tagState,
+			...tags,
+		};
 		
 		return {
-			body: text.map(val => html`<p>${val}</p>`),
+			body,
+			tags: this.tagState,
 			choiceList: inkStory.currentChoices,
-			tagState: {
-				...state.value.tagState,
-				...tagState,
-			}
 		};
 	}
 }
