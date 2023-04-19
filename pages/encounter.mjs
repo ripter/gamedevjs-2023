a5815df0617b97879f6be8c5d43583fc5f509c51
Chris <ripter001@gmail.com>
2023-04-19 03:05:47 +0000

    cleaning up from switching to new Story class

--- pages/encounter.mjs
+++ pages/encounter.mjs
@@ -1,28 +1,24 @@
 import { render, html } from '../libs/uhtml/index.mjs';
 import { signal, computed, effect } from '../libs/usignal.0.9.0.js';
 
-// import { loadStory } from '../utils/loadStory.mjs';
-// import { parseTags } from '../utils/parseTags.mjs';
 import { Story } from '../utils/Story.mjs';
 import { rollDice } from '../utils/rollDice.mjs';
 import { getChoiceComponent } from '../utils/getChoiceComponent.mjs';
-
-/* Page Variables */
-const state = signal({});
-let inkStory;
-
 
 /**
  * Page for Encounters.
  * Encounters serve as the central gameplay mechanism, providing players with an engaging challenge that tests their abilities, strategies, and luck, and ultimately determines their success or failure in the game.
 */
 export async function pageEncounter(selector, storyURL) {
+	const elm = document.querySelector(selector);
+	const state = signal({});
+	//
+	// Load the story
 	const story = await Story.load(storyURL, {
 		// rollDice function. Rolls dice and sets the "time" variable with the result.
 		rollDice: (skillName) => {
 			const skillLevel = window.player.skills[skillName];
 			const diceResults = rollDice(skillLevel);
-			// inkStory.variablesState['time'] = diceResults.reduce((acc, value) => {return acc + value}, 0);
 			story.setVariable('time',  diceResults.reduce((acc, value) => {return acc + value}, 0));
 		},	
 		// Updates the Ship
@@ -40,41 +36,13 @@ export async function pageEncounter(selector, storyURL) {
 			console.log('onGameOver');
 		},
 	});
-	// inkStory = await loadStory(storyURL);
-	// window.inkStory = inkStory; // for debugging
-	//
-	// inkStory.BindExternalFunction('rollDice', (skillName) => {
-	// 	const skillLevel = window.player.skills[skillName];
-	// 	const diceResults = rollDice(skillLevel);
-	// 	inkStory.variablesState['time'] = diceResults.reduce((acc, value) => {return acc + value}, 0);
-	// });
-	//
-	// Updates the Ship
-	// inkStory.BindExternalFunction('updateShip', (propName, deltaValue) => {
-	// 	console.log('Change Ship', propName, 'by', deltaValue);
-	// 	return "Ok damage the {freakin|strange|large} ship."	
-	// });
-	//
-	// Updates the Player
-	// inkStory.BindExternalFunction('updatePlayer', (propName, deltaValue) => {
-	// 	console.log('Change Player', propName, 'by', deltaValue);
-	// 	return "Ok damage the {amazing|cool|awesome} player."	
-	// });
-	//
-	// Called when the Encounter is over.
-	// inkStory.BindExternalFunction('onGameOver', () => {
-	// 	console.log('onGameOver');
-	// });
 	
-	const elm = document.querySelector(selector);
-	elm.className = 'page-encounter';
-
-	// run the current story
+	// Start the story
 	state.value = story.next();
-	// runFlow();
 	
 	//
 	// Render the page.
+	elm.className = 'page-encounter';
 	effect(() => {
 		const { tags, body, choiceList, turnsLeft } = state.value;
 		const { choiceType, background, title } = tags;
@@ -84,7 +52,7 @@ export async function pageEncounter(selector, storyURL) {
 		render(elm, html`
 			<h1>Encounter: ${title}!</h1>
 			<div class='story-text'>
-				${body}	
+				${body.map(text => html`<p>${text}</p>`)}	
 			</div>
 			<ul class=${`choice-list --count-${choiceList.length}`}>
 				${choiceList.map(item => Choice(item, (choiceIdx) => {
@@ -94,38 +62,3 @@ export async function pageEncounter(selector, storyURL) {
 		`);
 	});	
 }
-
-
-/**
- * Clicking on a choice
-*/
-// function handleChoiceClick(choiceIdx) {
-// 	// Select the choice
-// 	inkStory.ChooseChoiceIndex(choiceIdx);
-// 	// Run the Story
-// 	runFlow();
-// }
-
-
-/**
- * Run The InkStory
-*/
-// function runFlow() {
-// 	const text = [];
-// 	const tagList = [];	
-// 	
-// 	while (inkStory.canContinue) {
-// 		text.push(inkStory.Continue());
-// 		tagList.push(...inkStory.currentTags);
-// 	}
-// 	const tagState = parseTags(tagList);
-// 	
-// 	state.value = {
-// 		body: text.map(val => html`<p>${val}</p>`),
-// 		choiceList: inkStory.currentChoices,
-// 		tagState: {
-// 			...state.value.tagState,
-// 			...tagState,
-// 		}
-// 	};
-// }
